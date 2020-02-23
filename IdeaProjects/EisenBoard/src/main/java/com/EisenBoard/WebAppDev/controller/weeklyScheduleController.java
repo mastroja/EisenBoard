1c4ee7dd9b5ac6143a828bd09ea57e614f6dee6d
Hope Wilkinson <wilkinhj@mail.uc.edu>
2020-02-23 14:54:30 -0500

    Revert "Merge branch 'master' of https://github.com/jnoftz/EisenBoard"
    This reverts commit 7a9b484497c5503492bbeec93aa32526f6db188a, reversing
    changes made to 360f3f784ecc7964835232116da45ecaaab5ac5d.

--- IdeaProjects/EisenBoard/src/main/java/com/EisenBoard/WebAppDev/controller/weeklyScheduleController.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.EisenBoard.WebAppDev.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class weeklyScheduleController {
-
-
-}
--- IdeaProjects/EisenBoard/src/main/java/com/EisenBoard/WebAppDev/service/weeklyScheduleServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.EisenBoard.WebAppDev.service;
-
-import main.java.com.EisenBoard.WebAppDev.model.Activity;
-
-public class weeklyScheduleServiceImpl implements weeklyScheduleService {
-
-}
